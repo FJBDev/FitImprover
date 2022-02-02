0ced30ac83f2f9a4678cb0329e2c35635bc5200d
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-02-02 23:28:47 +0000

    Converting back to a fit file

--- src/FitImprover.java
+++ src/FitImprover.java
@@ -1,10 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FitImprover {
+
+
+		 private static File fitCSVTool = new File(FitImprover.class.getResource("FitCSVTool.jar").getFile());
+
+
 	 public static void main(String []args) {
 
 		 if(args.length == 0)
@@ -13,21 +18,46 @@ public class FitImprover {
 			 return;
 		 }
 
-		 File fitCSVTool = new File(FitImprover.class.getResource("FitCSVTool.jar").getFile());
 		 if(!fitCSVTool.exists())
 		 {
 			 System.out.println("FitCSVTool.jar could not be found. Aborting");
 			 return;
 		 }
 
-		 File fileToConvert = new File(FitImprover.class.getResource(args[0]).getFile());
-		 ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", fitCSVTool.getAbsolutePath(),fileToConvert.getAbsolutePath());
-		 try {
-			Process process = processBuilder.start();
+		 String fileName = args[0];
+		 //Convert the FIT file to CSV
+		 convertFitToCsv(fileName);
 
-			File commands = new File("C:\\Users\\frederic\\git\\FitImprover\\src\\commmands");
-			File dirOut = new File("C:\\Users\\frederic\\git\\FitImprover\\src\\dirout");
-			File dirErr = new File("C:\\Users\\frederic\\git\\FitImprover\\src\\direrr");
+		 //Remove the data that will prevent from converting back to FIT format
+
+		 //Add the power values based on the speed values
+
+		 //Add the power average values
+
+		 //Convert the CSV file to FIT file
+		 //file-withpower.fit"
+
+		 String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
+		 convertCsvToFit(fileNameWithoutExtension+ "-withPower");
+	   }
+
+	private static void convertCsvToFit(String fileNameWithoutExtension) {
+
+		 File csvFileToConvert = new File(FitImprover.class.getResource(fileNameWithoutExtension+".csv").getFile());
+
+		 Path path = Paths.get(csvFileToConvert.getAbsolutePath());
+		 String directory = path.getParent().toString();
+		 File convertedFitFile = new File(directory , fileNameWithoutExtension + ".fit");
+		 
+		 ProcessBuilder processBuilder = new ProcessBuilder(
+				 "java", 
+				 "-jar", 
+				 fitCSVTool.getAbsolutePath(), 
+				 "-c",
+				 csvFileToConvert.getAbsolutePath(), 
+				 convertedFitFile.getAbsolutePath());
+		 try {
+		Process process=	processBuilder.start();
 
 		    InputStream in = process.getInputStream();
 		    InputStream err = process.getErrorStream();
@@ -39,9 +69,34 @@ public class FitImprover {
 		    byte c[]=new byte[err.available()];
 		    err.read(c,0,c.length);
 		    System.out.println(new String(c));
-		    
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	   }
+
+	}
+
+	private static void convertFitToCsv(String fileName) {
+
+		 File fileToConvert = new File(FitImprover.class.getResource(fileName).getFile());
+
+		 ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", fitCSVTool.getAbsolutePath(),fileToConvert.getAbsolutePath());
+		 try {
+			processBuilder.start();
+
+//		    InputStream in = process.getInputStream();
+//		    InputStream err = process.getErrorStream();
+//
+//		    byte b[]=new byte[in.available()];
+//		    in.read(b,0,b.length);
+//		    System.out.println(new String(b));
+//
+//		    byte c[]=new byte[err.available()];
+//		    err.read(c,0,c.length);
+//		    System.out.println(new String(c));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
